7e276164385fe4056d8a5fa1849bca49605c06c8
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-10-16 00:23:44 -0600

    Inicio

---
+++ AeropuertoCliente/src/main/java/org/una/aerolinea/cliente/controller/LoginController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aerolinea.cliente.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jeffry
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private PasswordField txtContrasena;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void actIngresar(ActionEvent event) {
+    }
+    
+}
---
+++ AeropuertoCliente/src/main/java/org/una/aerolinea/cliente/controller/MenuPrincipalController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aerolinea.cliente.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jeffry
+ */
+public class MenuPrincipalController implements Initializable {
+
+    @FXML
+    private TitledPane menu;
+    @FXML
+    private Label lblUsuario;
+    @FXML
+    private AnchorPane apContenedor;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
