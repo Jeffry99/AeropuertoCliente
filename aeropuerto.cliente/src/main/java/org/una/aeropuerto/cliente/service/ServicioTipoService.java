804d72a3914f27c1792ee377741ce88f38df2a5d
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-10-22 02:19:23 -0600

    Services y Vistas Administración de Áreas de Trabajo

---
+++ aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/service/ServicioTipoService.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuerto.cliente.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.GenericType;
+import org.una.aerolinea.cliente.conexion.ConexionService;
+import org.una.aerolinea.cliente.dto.ServicioTipoDTO;
+import org.una.aerolinea.cliente.util.Respuesta;
+
+/**
+ *
+ * @author Jeffry
+ */
+public class ServicioTipoService {
+    public Respuesta crear(ServicioTipoDTO servicioTipo){
+        try{
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/crear");
+            conexion.post(servicioTipo);
+            if(conexion.isError()){
+                System.out.println("Error creacion de tipo de servicio: "+conexion.getError());
+                return new Respuesta(false, conexion.getError(), "No se pudo crear el tipo de servicio");
+            }
+            ServicioTipoDTO result = (ServicioTipoDTO) conexion.readEntity(ServicioTipoDTO.class);
+            return new Respuesta(true, "TipoServicio", result);
+        }catch(Exception ex){
+            System.out.println("Excepcion creacion de tipo de servicio: "+ex.getMessage());
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta modificar(Long id, ServicioTipoDTO servicioTipo){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/modificar", "/{id}", parametros);
+            conexion.put(servicioTipo);
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "No se pudo modificar el tipo de servicio");
+            }
+            ServicioTipoDTO result = (ServicioTipoDTO) conexion.readEntity(ServicioTipoDTO.class);
+            return new Respuesta(true, "TipoServicio", result);
+        }catch(Exception ex){
+            System.out.println("Excepcion modificacion tipo de servicio: "+ex.getMessage());
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getById(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto", "/{id}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar un tipo de servicio por su id");
+            }
+            ServicioTipoDTO result = (ServicioTipoDTO) conexion.readEntity(ServicioTipoDTO.class);
+            return new Respuesta(true, "TipoServicio",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getAll(){
+        try{
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/");
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar todos los tipos de servicios");
+            }
+            List<ServicioTipoDTO> result = (List<ServicioTipoDTO>) conexion.readEntity(new GenericType<List<ServicioTipoDTO>>(){});
+            return new Respuesta(true, "TiposServicios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByEstado(boolean estado){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", estado);
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/list/estado", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los tipos de servicios por estado");
+            }
+            List<ServicioTipoDTO> result = (List<ServicioTipoDTO>) conexion.readEntity(new GenericType<List<ServicioTipoDTO>>(){});
+            return new Respuesta(true, "TiposServicios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByNombreAproximate(String nombre){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", nombre);
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/list/nombre", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los tipos de servicios por su nombre");
+            }
+            List<ServicioTipoDTO> result = (List<ServicioTipoDTO>) conexion.readEntity(new GenericType<List<ServicioTipoDTO>>(){});
+            return new Respuesta(true, "TiposServicios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByDescripcionAproximate(String descripcion){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", descripcion);
+            ConexionService conexion = new ConexionService("tipos_servicios_aeropuerto/list/descripcion", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los tipos de servicios por la descripcion");
+            }
+            List<ServicioTipoDTO> result = (List<ServicioTipoDTO>) conexion.readEntity(new GenericType<List<ServicioTipoDTO>>(){});
+            return new Respuesta(true, "TiposServicios",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+}
---
+++ aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/service/TrabajoEmpleadoService.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuerto.cliente.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.GenericType;
+import org.una.aeropuerto.cliente.conexion.ConexionService;
+import org.una.aeropuerto.cliente.dto.TrabajoEmpleadoDTO;
+import org.una.aeropuerto.cliente.util.Respuesta;
+
+/**
+ *
+ * @author Jeffry
+ */
+public class TrabajoEmpleadoService {
+    public Respuesta crear(TrabajoEmpleadoDTO trabajoEmpleado){
+        try{
+            ConexionService conexion = new ConexionService("trabajos_empleados/crear");
+            conexion.post(trabajoEmpleado);
+            if(conexion.isError()){
+                System.out.println("Error creacion de trabajo empleado: "+conexion.getError());
+                return new Respuesta(false, conexion.getError(), "No se pudo crear el trabajo empleado");
+            }
+            TrabajoEmpleadoDTO result = (TrabajoEmpleadoDTO) conexion.readEntity(TrabajoEmpleadoDTO.class);
+            return new Respuesta(true, "TrabajoEmpleado", result);
+        }catch(Exception ex){
+            System.out.println("Excepcion creacion de trabajo empleado: "+ex.getMessage());
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    public Respuesta modificar(Long id, TrabajoEmpleadoDTO trabajoEmpleado){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            ConexionService conexion = new ConexionService("trabajos_empleados/modificar", "/{id}", parametros);
+            conexion.put(trabajoEmpleado);
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "No se pudo modificar el trabajo empleado");
+            }
+            TrabajoEmpleadoDTO result = (TrabajoEmpleadoDTO) conexion.readEntity(TrabajoEmpleadoDTO.class);
+            return new Respuesta(true, "TrabajoEmpleado", result);
+        }catch(Exception ex){
+            System.out.println("Excepcion modificacion de trabajo empleado: "+ex.getMessage());
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getById(Long id){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            ConexionService conexion = new ConexionService("trabajos_empleados", "/{id}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar el trabajo empleado por su id");
+            }
+            TrabajoEmpleadoDTO result = (TrabajoEmpleadoDTO) conexion.readEntity(TrabajoEmpleadoDTO.class);
+            return new Respuesta(true, "TrabajoEmpleado",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getAll(){
+        try{
+            ConexionService conexion = new ConexionService("trabajos_empleados/");
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar todos los trabajos empleados");
+            }
+            List<TrabajoEmpleadoDTO> result = (List<TrabajoEmpleadoDTO>) conexion.readEntity(new GenericType<List<TrabajoEmpleadoDTO>>(){});
+            return new Respuesta(true, "TrabajosEmpleados",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByEstado(boolean estado){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", estado);
+            ConexionService conexion = new ConexionService("trabajos_empleados/list/estado", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los trabajos empleados por el estado");
+            }
+            List<TrabajoEmpleadoDTO> result = (List<TrabajoEmpleadoDTO>) conexion.readEntity(new GenericType<List<TrabajoEmpleadoDTO>>(){});
+            return new Respuesta(true, "TrabajosEmpleados",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByEmpleado(Long empleado){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", empleado);
+            ConexionService conexion = new ConexionService("trabajos_empleados/list/empleado", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los empleados de un trabajo empleado");
+            }
+            List<TrabajoEmpleadoDTO> result = (List<TrabajoEmpleadoDTO>) conexion.readEntity(new GenericType<List<TrabajoEmpleadoDTO>>(){});
+            return new Respuesta(true, "TrabajosEmpleados",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+    
+    public Respuesta getByAreaTrabajo(Long areaTrabajo){
+        try{
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("term", areaTrabajo);
+            ConexionService conexion = new ConexionService("trabajos_empleados/list/area_trabajo", "/{term}", parametros);
+            conexion.get();
+            if(conexion.isError()){
+                return new Respuesta(false, conexion.getError(), "Error al buscar los empleados de un trabajo empleado");
+            }
+            List<TrabajoEmpleadoDTO> result = (List<TrabajoEmpleadoDTO>) conexion.readEntity(new GenericType<List<TrabajoEmpleadoDTO>>(){});
+            return new Respuesta(true, "TrabajosEmpleados",result);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "No puedo establecerce conexion con el servidor");
+        }
+    }
+}
