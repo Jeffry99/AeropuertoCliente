0c80021dbbc4d0a2077f751fe65f7e2e741294e5
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-11-13 00:19:49 -0600

    GeneradorReportes

--- aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/controller/ReportesHorasExtraController.java
+++ aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/controller/ReportesHorasExtraController.java
@@ -5,9 +5,40 @@
  */
 package org.una.aeropuerto.cliente.controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import org.una.aeropuerto.cliente.dto.AreaTrabajoDTO;
+import org.una.aeropuerto.cliente.dto.EmpleadoDTO;
+import org.una.aeropuerto.cliente.service.AreaTrabajoService;
+import org.una.aeropuerto.cliente.service.EmpleadoService;
+import org.una.aeropuerto.cliente.util.GenerarTransacciones;
+import org.una.aeropuerto.cliente.util.Mensaje;
+import org.una.aeropuerto.cliente.util.ReporteServicioRegistradoDTO;
+import org.una.aeropuerto.cliente.util.Respuesta;
 
 /**
  * FXML Controller class
@@ -16,12 +47,109 @@ import javafx.fxml.Initializable;
  */
 public class ReportesHorasExtraController implements Initializable {
 
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Label lblTipoBusqueda;
+    @FXML
+    private Button btnGenerar;
+    @FXML
+    private Button btnVolver;
+    @FXML
+    private Label horaInicio;
+    @FXML
+    private Label horaDeFin;
+    @FXML
+    private Label txtEstado;
+    @FXML
+    private DatePicker dpInicio;
+    @FXML
+    private DatePicker dpFinal;
+    @FXML
+    private ComboBox<Object> combobox;
+    @FXML
+    private CheckBox cbxEmpleado;
+    @FXML
+    private CheckBox cbxArea;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        combobox.setVisible(false);
     }    
+
+    @FXML
+    private void actGenerar(ActionEvent event) {
+        if(verificarCampos()){
+            //generarReporte();
+        }
+    }
+
+    @FXML
+    private void actVolver(ActionEvent event) {
+    }
+
+    @FXML
+    private void actEmpleado(ActionEvent event) {
+        cbxArea.setSelected(false);
+        combobox.getItems().clear();
+        combobox.setVisible(true);
+        lblTipoBusqueda.setText("Empleado:");
+        combobox.setPromptText("Seleccionar Empleado");
+        initEmpleados();
+    }
+
+    @FXML
+    private void actArea(ActionEvent event) {
+        cbxEmpleado.setSelected(false);
+        combobox.getItems().clear();
+        combobox.setVisible(true);
+        lblTipoBusqueda.setText("Área:");
+        combobox.setPromptText("Seleccionar Área");
+        initAreasTrabajo();
+    }
+    
+    private void initEmpleados(){
+        EmpleadoService empleadoService = new EmpleadoService();
+        ArrayList<EmpleadoDTO> empleados = new ArrayList<EmpleadoDTO>();
+        Respuesta respuesta = empleadoService.getByEstado(true);
+        if(respuesta.getEstado()){
+            empleados = (ArrayList<EmpleadoDTO>) respuesta.getResultado("Empleados");
+            ObservableList items = FXCollections.observableArrayList(empleados);
+            combobox.setItems(items);
+        }
+    }
+    
+    private void initAreasTrabajo(){
+        AreaTrabajoService areaService = new AreaTrabajoService();
+        ArrayList<AreaTrabajoDTO> areas = new ArrayList<AreaTrabajoDTO>();
+        Respuesta respuesta = areaService.getByEstado(true);
+        if(respuesta.getEstado()){
+            areas = (ArrayList<AreaTrabajoDTO>) respuesta.getResultado("AreasTrabajos");
+            ObservableList items = FXCollections.observableArrayList(areas);
+            combobox.setItems(items);
+        }
+    }
+    
+    private boolean verificarCampos(){
+        if(combobox.getValue() == null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Faltan datos por ingresar", "Por favor seleccione el área de trabajo o empleado");
+            return false;
+        }
+        if(dpInicio.getValue() == null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Faltan datos por ingresar", "Por favor seleccione la fecha de inicio");
+            return false;
+        }
+        if(dpFinal.getValue() == null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Faltan datos por ingresar", "Por favor seleccione la fecha de finalización");
+            return false;
+        }
+        return true;
+    }
+    
+   
     
 }
--- aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/util/Busqueda.java
+++ aeropuerto.cliente/src/main/java/org/una/aeropuerto/cliente/util/Busqueda.java
@@ -5,14 +5,10 @@
  */
 package org.una.aeropuerto.cliente.util;
 
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 import org.una.aeropuerto.cliente.dto.ServicioRegistradoDTO;
+import org.una.aeropuerto.cliente.dto.VueloDTO;
 import org.una.aeropuerto.cliente.service.ServicioRegistradoService;
 
 /**
@@ -21,17 +17,17 @@ import org.una.aeropuerto.cliente.service.ServicioRegistradoService;
  */
 public class Busqueda {
  
-    private ServicioRegistradoService servicioRegistradoService = new ServicioRegistradoService();
+    private final ServicioRegistradoService servicioRegistradoService = new ServicioRegistradoService();
     ArrayList<ServicioRegistradoDTO> servicios;
     ArrayList<ServicioRegistradoDTO> serviciosFechas = new ArrayList();
+    ArrayList<VueloDTO> vuelosFechas = new ArrayList();
      
-
-    public ArrayList<ServicioRegistradoDTO> busquedaRangoFechas(Date menor, Date mayor, ArrayList<ServicioRegistradoDTO> listaFechas){
-        if(!listaFechas.isEmpty()){
-            for(int i=0; i<listaFechas.size(); i++){
-                if(menor.before(listaFechas.get(i).getFechaRegistro())){
-                    if(mayor.after(listaFechas.get(i).getFechaRegistro())){
-                        serviciosFechas.add(listaFechas.get(i));
+    public ArrayList<ServicioRegistradoDTO> busquedaRangoRecaudacion(Date menor, Date mayor, ArrayList<ServicioRegistradoDTO> lista){
+        if(!lista.isEmpty()){
+            for(int i=0; i<lista.size(); i++){
+                if(menor.before(lista.get(i).getFechaRegistro())){
+                    if(mayor.after(lista.get(i).getFechaRegistro())){
+                        serviciosFechas.add(lista.get(i));
                     }
                 }
             }  
@@ -40,4 +36,19 @@ public class Busqueda {
         }
         return serviciosFechas;
     }
+    public ArrayList<VueloDTO> busquedaRangoRecorridos(Date menor, Date mayor, ArrayList<VueloDTO> lista){
+        if(!lista.isEmpty()){
+            for(int i=0; i<lista.size(); i++){
+                if(menor.before(lista.get(i).getFecha())){
+                    if(mayor.after(lista.get(i).getFecha())){
+                        vuelosFechas.add(lista.get(i));
+                    }
+                }
+            }  
+        }else{
+            System.out.println("No hay vuelos registrados");
+        }
+        return vuelosFechas;
+    }
 }
+
